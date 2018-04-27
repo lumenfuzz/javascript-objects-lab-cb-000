9f660493b46d4e71ccca2319606ec303438bcad6
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-27 14:41:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
-function deleteFromObjectByKey(object, key) {
+function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key
 }
