91a9f5a5e21674c143966111204b869e4912c1bd
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-27 14:38:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var object_clone = {}
-  object_clone[object.key] = object.value
-  object_clone[key] = value
-  return object_clone
+  return Object.assign({}, object, { [key]: value })
 }
